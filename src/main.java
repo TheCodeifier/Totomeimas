97733a7adb62eae487dd215d0e3288b7db4a5a2a
Diomuzan <94070451+TheCodeifier@users.noreply.github.com>
2025-06-10 11:49:40 +0200

    first commit

---
+++ src/main.java
@@ -0,0 +1,33 @@
+class student {
+    public String name;
+    public String study;
+    public String interests;
+
+    String getName() {
+        return name;
+    }
+
+    String getStudy() {
+        return study;
+    }
+
+    String getInterests() {
+        return interests;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+     public void setStudy(String study) {
+        this.study = study;
+    }
+
+     public void setInterests(String interests) {
+        this.interests = interests;
+    }
+}
+
+class main {
+    
+}
